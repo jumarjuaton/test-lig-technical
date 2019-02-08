4779ad026470e5696c84631bb57e1db0dae6b649
Jumar Juaton <dev.jumarjuaton@gmail.com>
2019-02-08 17:47:40 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="466" height="72" src="https://i.ibb.co/bKRG5R4/logo.png" />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/pages/index";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
